6a8a13e0035b99305453775ef448c12ab6cf0587
osamanathmi <osamanathmi@gmail.com>
2026-06-04 01:41:13 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SdkCore",
-            url: "https://github.com/jitbackClassera/LMS-iOS-AI-KMP-Releases/releases/download/1.0.2/SdkCore.xcframework.zip",
+            url: "https://github.com/jitbackClassera/LMS-iOS-AI-KMP-Releases/releases/download/1.0.3/SdkCore.xcframework.zip",
             checksum: "2b6697cf56f383a8493d68e60d821cad08127896d3423a25461b8502779f475b"
         )
     ]
