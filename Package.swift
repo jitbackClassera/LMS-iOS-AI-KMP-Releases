5319a18abb0c93e95253c5eb4af7744e28d7c68b
osamanathmi <osamanathmi@gmail.com>
2026-06-04 00:27:07 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "AiSDK",
+    name: "AISDK",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "AiSDK", targets: ["AiSDK"])
+        .library(name: "AISDK", targets: ["AISDK"])
     ],
     targets: [
         .binaryTarget(
-            name: "AiSDK",
+            name: "AISDK",
             url: "https://github.com/jitbackClassera/LMS-iOS-AI-KMP-Releases/releases/download/1.0.0/SdkCore.xcframework.zip",
             checksum: "e61c09442bcfa33c3e43488b6789bee6832f321d475219d5b95da125e62ac5aa"
         )
