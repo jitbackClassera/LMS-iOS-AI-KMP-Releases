8dd3b160a1883be5d07a178e1a3e1ec4f52cb9d0
jitbackClassera <Jitpack@classera.com>
2026-06-03 23:22:41 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,17 @@
+// swift-tools-version: 5.9
+import PackageDescription
 
+let package = Package(
+    name: "ChatSdk",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "ChatSdk", targets: ["ChatSdk"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ChatSdk",
+            url: "https://github.com/jitbackClassera/LMS-iOS-KMP-Releases/releases/download/1.0.0/ChatSdk.xcframework.zip",
+            checksum: "74ac00dc97fddcdbe0d7bdc2b198e45a2c6e251fc6751cc2dc515b5b36162704"
+        )
+    ]
+)
