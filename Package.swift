b95c41218c161b4e786f0f72855c2b2607981925
osamanathmi <osamanathmi@gmail.com>
2026-06-05 16:15:01 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AILms",
-            url: "https://github.com/jitbackClassera/LMS-iOS-AI-KMP-Releases/releases/download/1.0.4/AILms.xcframework.1.zip",
+            url: "https://github.com/jitbackClassera/LMS-iOS-AI-KMP-Releases/releases/download/1.0.4/AILms.xcframework.zip",
             checksum: "a92142d3e81ccad66971896f9cc6c587c56391428e3465bd19463fb14785005e"
         )
     ]
